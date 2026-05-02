f8badb5f0d44abb8ae340691eb2180353e0834c3
Marcin Suder <marcin.code.github@gmail.com>
2026-05-03 00:38:00 +0200

    add endpoints for mongoDB app

---
+++ routes/employees.routes.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+const router = express.Router();
+
+router.get('/employees', (req, res) => {
+  req.db.collection('employees')
+    .find()
+    .toArray()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.get('/employees/random', (req, res) => {
+  req.db.collection('employees')
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray()
+    .then(data => res.json(data[0]))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.get('/employees/:id', (req, res) => {
+  req.db.collection('employees')
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then(data => {
+      if (!data) res.status(404).json({ message: 'Not found' });
+      else res.json(data);
+    })
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.post('/employees', (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  req.db.collection('employees')
+    .insertOne({ firstName, lastName })
+    .then(() => res.json({ message: 'OK' }))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.put('/employees/:id', (req, res) => {
+  const { firstName, lastName } = req.body;
+
+  req.db.collection('employees')
+    .updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $set: { firstName, lastName } }
+    )
+    .then(() => res.json({ message: 'OK' }))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.delete('/employees/:id', (req, res) => {
+  req.db.collection('employees')
+    .deleteOne({ _id: ObjectId(req.params.id) })
+    .then(() => res.json({ message: 'OK' }))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+module.exports = router;
---
+++ routes/products.routes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+const router = express.Router();
+
+router.get('/products', (req, res) => {
+  req.db.collection('products')
+    .find()
+    .toArray()
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.get('/products/random', (req, res) => {
+  req.db.collection('products')
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray()
+    .then(data => res.json(data[0]))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.get('/products/:id', (req, res) => {
+
+  req.db.collection('products')
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then(data => {
+      if (!data) res.status(404).json({ message: 'Not found' });
+      else res.json(data);
+    })
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.post('/products', (req, res) => {
+  const { name, client } = req.body;
+
+  req.db.collection('products')
+    .insertOne({ name, client })
+    .then(() => res.json({ message: 'OK' }))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.put('/products/:id', (req, res) => {
+  const { name, client } = req.body;
+
+  req.db.collection('products')
+    .updateOne(
+      { _id: ObjectId(req.params.id) },
+      { $set: { name, client } }
+    )
+    .then(() => res.json({ message: 'OK' }))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+router.delete('/products/:id', (req, res) => {
+  req.db.collection('products')
+    .deleteOne({ _id: ObjectId(req.params.id) })
+    .then(() => res.json({ message: 'OK' }))
+    .catch(err => res.status(500).json({ message: err }));
+});
+
+module.exports = router;
